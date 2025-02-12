83b3778a6259571b98e0b4bf9633868c512766d4
3moJunioR <m.wa7ba@gmail.com>
2025-02-12 02:54:49 +0200

    Transaction&TransactionController

--- obj/Debug/net6.0/Expense Tracker.AssemblyInfo.cs
+++ obj/Debug/net6.0/Expense Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5adfceccc7e7c3f06a252184e7ad3cd16631605b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a68a49761519ade1d75228e4a93a103077103b89")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
